29db550a55e0179fa1bcc729cea11ffff1ffe1cf
ZoeHenBoz <zoehenboz20@gmail.com>
2024-02-26 07:12:06 -0500

    HOLA

--- src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
+++ src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
@@ -26,6 +26,7 @@ public class Calculadora {
                 // Operación división:
                 respuesta = (double) numero1 / numero2;
                 break;
+			HOLA
             
             // TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO.
                 
